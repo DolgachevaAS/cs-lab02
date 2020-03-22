feb22cd367b034b24cb2ea4ce8141c7e0793e436
Bob (DolgachevaAS) <bob@example.com>
2020-03-22 17:13:57 +0300

    code: произведение чисел

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ cout<<"Enter A and B";
 int a, b;
 cin>>a>>b;
 cout<<"A+B="<<a+b<<'\n';
-cout<< "A-B="<<a+b<</'n';
-cout<< "A*B="<<a*b<</'n';
-
+cout<< "A-B="<<a-b<<'\n';
+cout<< "3A*B="<<3*a*b<<'\n';// proisvedenie
     return 0;
 }
