48a9756b2ea1d902e2c85f115b26a608e4af4203
Bob (DolgachevaAS) <bob@example.com>
2020-03-31 10:02:03 +0300

    code: вывод минимума

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-cout<<"Enter A and B";
+cout<<"Enter A and B"<<endl;
 int a, b;
 cin>>a>>b;
 cout<<"A+B="<<a+b<<'\n';
@@ -13,9 +13,9 @@ cout<< "3A*B="<<3*a*b<<'\n';// proisvedenie
 cout<< "A/B="<<a/b<<'\n';
 if (a>b)
 {
-    cout<<"max="<<a<<'/n';//вывод максимума
+    cout<<"min="<<a<<'/n';
 }
 else
-{cout<<"max="<<b<<endl;}
+{cout<<"min="<<b<<endl;}
     return 0;
 }
